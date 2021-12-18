a29c358dd96f2923579e86a8d76bf363a5b0173f
Defender007 <mydjangopythonprojects@gmail.com>
2021-12-18 12:33:05 +0100

    feat:add post and common models

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdming(admin.ModelAdmin):
+    list_display = ["author", "title", "slug", "published_status"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostAdming)
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import uuid
+from django.utils.translation import gettext_lazy as _
+from blog.settings.base import AUTH_USER_MODEL
+from apps.common.models import TImeStampedModel
+from autoslug import AutoSlugField
 
-# Create your models here.
+
+class PostPublishedManager(models.Model):
+    def get_queryset(self):
+        return (
+            super(PostPublishedManager, self)
+            .get_deferred_fields()
+            .filter(published_status=True)
+        )
+
+class Post(TImeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True,
+                          blank=False, editable=False)
+    author= models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(_("Title"), max_length=255)
+    slug= AutoSlugField(populate_from="title", always_update=True, unique=True)
+    content = models.TextField(_("Post Content"))
+    published_status = models.BooleanField(_("Published Status"), default=False)
+    
+    objects=models.Manager()
+    published = PostPublishedManager()
+    
+    def __str__(self) -> str:
+        return f"{self.title}"
+    
+    def save(self):
+        """The title method returns a string where the first 
+        character in every word is uppercase
+        """
+        self.title =str.title(self.title)
+        self.content= str.capitalize(self.content)
+        super(Post, self).save(*args, **kwargs)
+        
